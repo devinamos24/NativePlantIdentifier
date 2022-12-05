1719cfdfb782a137ac49aea1289ca73f01910c74
devina1 <78060983+devina1@users.noreply.github.com>
2022-12-05 07:18:43 -0600

    Fix typo in PlantDatabase.kt

--- app/src/main/java/com/jinxservers/nativeplantidentifier/database/PlantDatabase.kt
+++ app/src/main/java/com/jinxservers/nativeplantidentifier/database/PlantDatabase.kt
@@ -4,6 +4,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [ PlantEntity::class ], version = 1)
-abstract class CrimeDatabase : RoomDatabase() {
+abstract class PlantDatabase : RoomDatabase() {
     abstract fun plantDao(): PlantDao
 }

93527c9f1e1538d88947936dd8ba15bf6210c334
devina1 <78060983+devina1@users.noreply.github.com>
2022-12-05 07:16:42 -0600

    Rename PlantIdentifierScreen to IdentifierScreen

--- app/src/main/java/com/jinxservers/nativeplantidentifier/MainActivity.kt
+++ app/src/main/java/com/jinxservers/nativeplantidentifier/MainActivity.kt
@@ -10,14 +10,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.jinxservers.nativeplantidentifier.ui.PlantIdentifierScreen
+import com.jinxservers.nativeplantidentifier.ui.IdentifierScreen
 import com.jinxservers.nativeplantidentifier.ui.theme.NativePlantIdentifierTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        PlantRepository.initialize(this)
         setContent {
-            PlantIdentifierScreen()
+            IdentifierScreen()
         }
     }
 }

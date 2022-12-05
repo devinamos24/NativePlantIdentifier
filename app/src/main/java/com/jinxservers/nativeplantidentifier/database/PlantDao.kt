a3c882d3dde2873b65f189a4f1c6be3c1c40bd63
devina1 <78060983+devina1@users.noreply.github.com>
2022-12-05 07:18:15 -0600

    Add raw query for finding matching plants

--- app/src/main/java/com/jinxservers/nativeplantidentifier/database/PlantDao.kt
+++ app/src/main/java/com/jinxservers/nativeplantidentifier/database/PlantDao.kt
@@ -2,14 +2,19 @@ package com.jinxservers.nativeplantidentifier.database
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.RawQuery
+import androidx.sqlite.db.SupportSQLiteQuery
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PlantDao {
 
-    @Query("SELECT * FROM plantentity")
-    fun getPlants(): Flow<List<PlantEntity>>
+//    @Query("SELECT * FROM plants")
+//    suspend fun getPlants(): Flow<List<PlantEntity>>
 
-    @Query("SELECT COUNT(*) FROM plantentity")
-    fun getTotalPlants(): Int
+    @RawQuery
+    suspend fun getMatchingPlants(query: SupportSQLiteQuery): List<PlantEntity>
+
+//    @Query("SELECT COUNT(*) FROM plants")
+//    suspend fun getTotalPlants(): Int
 }

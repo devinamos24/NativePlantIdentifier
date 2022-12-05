f338840e2636525ca137a5b4d3e2aa0326e18613
devina1 <78060983+devina1@users.noreply.github.com>
2022-12-05 07:19:38 -0600

    Add necessary fields to PlantEntity.kt so that room can generate it correctly

--- app/src/main/java/com/jinxservers/nativeplantidentifier/database/PlantEntity.kt
+++ app/src/main/java/com/jinxservers/nativeplantidentifier/database/PlantEntity.kt
@@ -1,10 +1,19 @@
 package com.jinxservers.nativeplantidentifier.database
 
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "plant")
 data class PlantEntity(
     @PrimaryKey val scientificName: String,
-    val description: String
-)
+    val leaf_simple: Boolean,
+    val leaf_unlobed: Boolean,
+    val leaf_lobed: Boolean,
+    val leaf_compound: Boolean,
+    val leaf_digitate: Boolean,
+    val leaf_pinnate: Boolean,
+    val leaf_pinnately_trifoliate: Boolean,
+    val leaf_palmately_trifoliate: Boolean,
+    val leaf_bipinnate: Boolean,
+    )
